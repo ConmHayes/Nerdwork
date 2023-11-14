8f068ce41772ec4bdfbdf9d41a5d7937cd53e848
Bruno <brunoradoja2911@gmail.com>
2023-11-14 14:23:37 +0000

    fixed nav bar

--- client/src/components/NavigationBar/index.jsx
+++ client/src/components/NavigationBar/index.jsx
@@ -4,35 +4,37 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="light" expand="lg" className="mb-3">
-      <Navbar.Brand as={NavLink} to="/">
-        Nerdwork
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={NavLink} exact to="/home" activeClassName="nav-link-active">
-            Home
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/books" className={({ isActive }) => (isActive ? "nav-link-active" : "")}>
-            Books
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/comic_books" activeClassName="nav-link-active">
-            Comic Books
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/games" activeClassName="nav-link-active">
-            Games
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/profile" activeClassName="nav-link-active">
-            Profile
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/" activeClassName="nav-link-active">
-            Logout
-          </Nav.Link>
-          {/* Additional navigation links as needed */}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className='main-container mt-5'>
+      <Navbar bg="light" expand="lg" className="mb-3" fixed="top">
+        <Navbar.Brand as={NavLink} to="/">
+          Nerdwork
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/home" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/books" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Books
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/comic_books" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Comic Books
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/games" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Games
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/profile" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Profile
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/" className={({ isActive }) => isActive ? "nav-link-active" : "nav-link"}>
+              Logout
+            </Nav.Link>
+            {/* Additional navigation links as needed */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 };
 

2fe005e592438a8049e7029d9c7efed98a1d3b59
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-11-15 10:16:37 +0000

    going to request page

--- client/src/components/BookSearchCard/index.jsx
+++ client/src/components/BookSearchCard/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import "./bookCard.css"
 import { useNavigate } from 'react-router-dom';
 
-export default function BookCard ({ book }){
-    const { title, author, releaseDate, description } = book;
+export default function BookSearchCard ({ book }){
+    const { title, author, book_image,releaseDate } = book;
     const navigate = useNavigate();
 
     function displayUser(){
-       navigate(`/BookSearch/${title}`)
+       navigate(`/request`)
     }
 
     return (
         <div className="book-card" onClick={displayUser}>
+            <img src={book_image} alt={title}/> 
             <h3>{title}</h3>
             <p>Author: {author}</p>
             <p>Release Date: {releaseDate}</p>
-            <p>{description}</p>
         </div>
     );
 };

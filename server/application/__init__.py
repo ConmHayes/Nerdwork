d03d53287870ec4b0f8b85e4e4e07cef050f0708
Connor Hayes <conmhayes@gmail.com>
2023-11-15 14:49:57 +0000

    added in dotenv back

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -4,10 +4,11 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from .database.models import db
-
+from dotenv import load_dotenv
 #from routes import item
 from application.routes import user
 
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)

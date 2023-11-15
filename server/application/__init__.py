cee091772590126e618e7d03eb8f5bf7e43e9e7c
Connor Hayes <conmhayes@gmail.com>
2023-11-15 09:41:35 +0000

    neatened the applications __init__

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -8,24 +8,16 @@ from .database.models import db
 #from routes import item
 from application.routes import user
 
-
-# Loading dotenv
 load_dotenv()
-
-# Creating the flask app 
 app = Flask(__name__)
 CORS(app)
 
 db_url = os.environ.get("DB_URL")
 
-if not db_url:
-    raise ValueError("DB_URL env variable is not set")
-
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 # Create DB instance
 db.init_app(app)
-
 
 #Register Blueprints
 #app.register_blueprint(item)

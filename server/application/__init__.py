6c18f69f83a71e8081834b4fe903a4f0ecd01736
Connor Hayes <conmhayes@gmail.com>
2023-11-15 09:38:25 +0000

    Working Routes

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -1,16 +1,18 @@
 # Imports
 import os
 from dotenv import load_dotenv
-
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+from .database.models import db
+
+#from routes import item
+from application.routes import user
+
 
 # Loading dotenv
 load_dotenv()
 
 # Creating the flask app 
-
 app = Flask(__name__)
 CORS(app)
 
@@ -22,6 +24,9 @@ if not db_url:
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
 # Create DB instance
-db = SQLAlchemy(app)
+db.init_app(app)
 
-# from application import routes
+
+#Register Blueprints
+#app.register_blueprint(item)
+app.register_blueprint(user.user_bp)
---
+++ server/application/routes/item.py
@@ -0,0 +1,60 @@
+"""
+ All routes related to items, get book, post, get by category 
+"""
+# create, update, delete
+# Search in search bar:
+#     get product by name 
+
+# Filter search by category/genre:
+#     get product by category 
+
+# Add item to their portfolio:
+# @app.route("/product/", methods=["get", "POST", "delete", "patch"])
+# def create():
+#     if method == "post":
+#         ## do something
+    
+
+"""
+
+from flask import Blueprint, request, jsonify
+from application import db
+from database import Item
+
+item = Blueprint("item", __name__) 
+
+# Formatting the items 
+
+def format_item(item): 
+    return {
+        "product_type": item.product_type,
+        "name": item.name,
+        "user_id": item.user_id,
+        "category": item.category,
+        "platform": item.platform
+    }
+
+# Display all books or games or comics
+@item.route("/", methods=["GET"])
+def get_category():
+    if request.method == "GET":
+        # Assuming that data comes in the form e.g. {category: "books"}
+        data = request.json
+
+        # Querying the Item table by category
+        items = Item.query.filter_by(category=data)
+
+        item_list = []
+
+        for item in items:
+            item_list.append(format_item(item))
+
+        # Returning the data for the specified category
+
+        return {"items": item_list
+                }
+
+"""
+
+
+

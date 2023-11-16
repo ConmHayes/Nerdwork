d0ea037dc037778b2e70dd75f52fa1d21b20391d
David Evans <david@evanstmd.plus.com>
2023-11-16 14:09:05 +0000

    Working on profile/bookshelf

---
+++ client/src/pages/MyBookshelfPage/index.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react"
+import { BookCard } from "../../components"
+import { Link } from "react-router-dom";
+
+const apiURL = "https://nerdwork-server.onrender.com"
+const siteURL = "https://nerdwork.onrender.com/"
+const localURL = "http://localhost:5173/"
+
+
+export default function MyBookshelfPage( { sidebarExtended, setSidebarExtended }){
+    
+    const top_icons = ["home", "sports_esports", "import_contacts", "diversity_3"]
+    const bottom_icons = ["settings", "call"]
+
+
+    const top_links = [`${localURL}profile`, "/", "/", "/"]
+    const bottom_links = ["/", "/"]
+
+    const initialBooks = [
+        // give me random books
+        {
+          id: 1,
+          title: 'The Hobbit',
+          img: '',
+          author: 'J.R.R. Tolkien',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+        {
+          id: 2,
+          title: 'The Lord of the Rings',
+          img: '',
+          author: 'J.R.R. Tolkien',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+        {
+          id: 3,
+          title: 'Harry Potter and the Chamber of Secrets',
+          img: '',
+          author: 'J.K. Rowling',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+        {
+          id: 4,
+          title: 'Harry Potter and the Prisoner of Azkaban',
+          img: '',
+          author: 'J.K. Rowling',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+        {
+          id: 5,
+          title: 'Harry Potter and the Goblet of Fire',
+          img: '',
+          author: 'J.K. Rowling',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+        {
+          id: 6,
+          title: 'Harry Potter and the Order of the Phoenix',
+          img: '',
+          author: 'J.K. Rowling',
+          genres: ['Fantasy'],
+          owner: 'John Doe',
+          rating: 4.5
+        },
+      ];
+
+      
+
+    return (
+        
+        <div className="flexbox-container profile-container">
+
+            <div className="flexbox-item profile-sidebar">
+                <div className="flexbox-container profile-bar" style = {{width: "100%"}}>
+                    <div className="flexbox-container profile-header" style={{justifyContent: "center"}}>
+                        <div className="flexbox-item">
+                            <span className="dot">  
+                                <i className="material-icons ikon">person</i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flexbox-container option-box ">
+                    {top_icons.map((icon, i) => (
+                    <Link to={top_links[i]} className="link" key={i}>    
+                        <div className={`flexbox-item profile-box ${i % 2 === 0 ? 'even' : 'odd'}`}>
+                                <i className="material-icons ">{icon}</i>
+                        </div>
+                    </Link>
+                    ))}
+                </div>
+                <div className="flexbox-item placeholder" style={{border: "transparent"}}>
+
+                </div>
+                <div className="flexbox-item option-row">
+                    {bottom_icons.map((icon, i) => (
+                        <Link to={bottom_links[i]} className="link" key={i}>
+                            <div className={`flexbox-item profile-box ${i % 2 === 0 ? 'even' : 'odd'}`}>
+                                    <i className="material-icons">{icon}</i> 
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <div className="flexbox-container profile-bookshelf">
+                <p>Your Books</p>
+                <div className="flexbox-item carousel-container" style={{justifyContent:"flex-start"}}>
+                    {
+                    initialBooks.map((book, i) => (
+                        <div key={i}><BookCard book={ book } /></div>
+                    ))}
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react"
 import React from "react"
 import "./style.css"
+import { Link } from "react-router-dom"
 
 const apiURL = "https://nerdwork-server.onrender.com"
 const siteURL = "https://nerdwork.onrender.com/"
+const localURL = "http://localhost:5173/"
 
 export default function ProfilePage(){
-    //const [sidebarExtended, setSidebarExtended] = useState(true)
+    const [sidebarExtended, setSidebarExtended] = useState(true)
     const [username, setUsername] = useState("")
 
     async function getUsername(){
@@ -20,6 +22,7 @@ export default function ProfilePage(){
         }
         const response = await fetch(`${apiURL}/user/${localStorage.email}`, options)
         const data = await response.json()
+        console.log(response)
         setUsername(data.username)
       }
       
@@ -27,8 +30,11 @@ export default function ProfilePage(){
     
     const top_rows = ["My Bookshelf", "My Games", "My Comics", "My Friends"]
     const top_icons = ["book", "sports_esports", "import_contacts", "diversity_3"]
+    const top_links = [`${localURL}profile/bookshelf`, "/", "/", "/"]
+
     const bottom_rows = ["Settings", "Contact Us"]
     const bottom_icons = ["settings", "call"]
+    const bottom_links = ["/", "/"]
 
     useEffect(() => {
         const carousel = document.querySelector("#carouselExampleSlidesOnly")
@@ -68,10 +74,12 @@ export default function ProfilePage(){
 
                 <div className="flexbox-container option-row ">
                     {top_rows.map((title, i) => (
-                        <div className={`flexbox-item profile-option ${i % 2 === 0 ? 'even' : 'odd'}`} key={i}>
-                            <i className="material-icons left">{top_icons[i]}</i>
-                            {title}
+                    <Link to={top_links[i]} className="link" key={i}>    
+                        <div className={`flexbox-item profile-option ${i % 2 === 0 ? 'even' : 'odd'}`}>
+                                <i className="material-icons left">{top_icons[i]}</i>
+                                {title}
                         </div>
+                        </Link>
                     ))}
                 </div>
                 <div className="flexbox-item placeholder">
@@ -79,10 +87,16 @@ export default function ProfilePage(){
                 </div>
                 <div className="flexbox-item option-row">
                     {bottom_rows.map((title, i) => (
-                        <div className={`flexbox-item profile-option ${i % 2 === 0 ? 'even' : 'odd'}`} key={i}>
-                            <i className="material-icons left">{bottom_icons[i]}</i>
-                            {title}
-                        </div>
+                        <Link to={bottom_links[i]} className="link" key={i}>
+                            <div className={`flexbox-item profile-option ${i % 2 === 0 ? 'even' : 'odd'}`}>
+                                
+                                    <i className="material-icons left">{bottom_icons[i]}</i>
+                                    {title}
+                                
+                            </div>
+                        </Link>
+                        
+                        
                     ))}
                 </div>
             </div>

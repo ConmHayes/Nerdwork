c4a04fd99c70dce1fdad6caa2b26b453ba97d790
IldikoMagda <ildiko.magda@gmail.com>
2023-11-16 10:29:29 +0000

    email route protected

--- server/application/routes/user.py
+++ server/application/routes/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from application.database.models import User
+from auth_middleware import token_required
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
 
@@ -25,7 +26,9 @@ def get_users():
 #     user = User.query.filter_by(user_id=user_id).first()
 #     return jsonify(id=user.user_id, username=user.username, address=user.address, email=user.email, password=user.password)
 
+##this is /user/email
 @user_bp.route('/<email>', methods=['GET'])
+@token_required
 def get_user(email):
     user = User.query.filter_by(email=email).first()
     return jsonify(user_id=user.user_id, username=user.username, address=user.address, email=user.email, password=user.password)

4a008f230d84405c92742d528018bc4c42991778
mgr-mirnal <mgr.mrinal97@gmail.com>
2023-11-16 10:56:10 +0000

    changing routes

--- client/src/components/BookSearchCard/index.jsx
+++ client/src/components/BookSearchCard/index.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import "./bookCard.css"
 import { useNavigate } from 'react-router-dom';
 
+
 export default function BookSearchCard ({ book }){
     const { title, author, book_image,releaseDate } = book;
     const navigate = useNavigate();
 
-    function displayUser(){
-       navigate(`/request`)
+    function displayUser(id){
+       navigate(`/request/${id}`)
     }
 
     return (
-        <div className="book-card" onClick={displayUser}>
+        <div className="book-card" onClick={() => displayUser(book.id)}>
             <img src={book_image} alt={title}/> 
             <h3>{title}</h3>
             <p>Author: {author}</p>

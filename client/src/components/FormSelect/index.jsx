6fa94af6974a2efb07a5ac949aa3eedeb397a200
Bruno <brunoradoja2911@gmail.com>
2023-11-20 01:15:33 +0000

    fixed form page for new item

--- client/src/components/FormSelect/index.jsx
+++ client/src/components/FormSelect/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
-const FormSelect = ({ label, name, value, options, onChange }) => {
+const FormSelect = ({ label, name, value, options, onChange, disabled = false }) => {
   return (
     <Form.Group className="mb-3">
       <Form.Label>{label}</Form.Label>
-      <Form.Control as="select" name={name} value={value} onChange={onChange}>
-        <option value="">Select...</option>
+      <Form.Control as="select" name={name} value={value} onChange={onChange} disabled={disabled}>
+        <option value="" disabled style={{ color: '#666' }}>Select...</option> {/* Unselectable and darker */}
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
--- client/src/pages/FormsPage/index.jsx
+++ client/src/pages/FormsPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button, Form, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { FormInput, FormMultiSelect, FormRating, FormSelect, NavigationBar } from '../../components';
@@ -18,41 +18,48 @@ const FormsPage = ({ onAddBook }) => {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
+  useEffect(() => {
+    // Log formData whenever it changes
+    console.log("Form data updated:", formData);
+  }, [formData]); // Dependency array includes formData
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Basic fetch call for testing
+  
+    // Prepare the data to be sent
+    const dataToSend = {
+      ...formData,
+      genre: selectedgenre, 
+      issue_num: parseInt(formData.issue_num, 10), 
+      user_id: parseInt(formData.user_id, 10), 
+      rating: parseFloat(formData.rating) 
+    };
+  
     try {
-      const response = await fetch('https://nerdwork-server.onrender.com/item', {
+      const response = await fetch('https://nerdwork-server.onrender.com/item/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          title: "Test Title",
-          img: "test",
-          author: "Test Author",
-          genre: "[Test Genre]",
-          issue_num: 1,
-          user_id: 1,
-          rating: 5,
-          category: "book"
-        }),
+        body: JSON.stringify(dataToSend),
       });
-
+  
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        const errorBody = await response.json(); // Assuming the server sends a JSON response
+        throw new Error(`HTTP error! status: ${response.status}, Message: ${errorBody.message}`);
       }
-
+      
+  
       const result = await response.json();
       console.log(result);
-      // onAddBook(result);
+      onAddBook(result);
       // navigate("/profile");
     } catch (error) {
       setError(`There was a problem adding your item: ${error.message}`);
       console.error('Error:', error);
     }
   };
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -68,7 +75,7 @@ const FormsPage = ({ onAddBook }) => {
   return (
     <Container>
       <NavigationBar />
-      <Row className="justify-content-md-center">
+      <Row className="justify-content-md-center w-100">
         <Col md={6}>
           <h2>Add New Item</h2>
           {error && <Alert variant="danger">{error}</Alert>}
@@ -76,8 +83,9 @@ const FormsPage = ({ onAddBook }) => {
             <FormInput label="Title" type="text" placeholder="Enter title" name="title" value={formData.title} onChange={handleChange} />
             <FormInput label="Image URL" type="text" placeholder="Enter image URL" name="img" value={formData.img} onChange={handleChange} />
             <FormInput label="Author" type="text" placeholder="Enter author's name" name="author" value={formData.author} onChange={handleChange} />
-            <FormMultiSelect label="genre" name="genre" selected={selectedgenre} options={['Fiction', 'Fantasy', 'Adventure']} onChange={handleGenreChange} />
-            <FormInput label="Owner" type="text" placeholder="Enter owner's username" name="owner" value={formData.user_id} onChange={handleChange} />
+            <FormMultiSelect label="Genres" name="genre" selected={selectedgenre} options={['Cyberpunk', 'Superhero', 'Romance', 'Adventure', 'Thriller', 'Survival', 'Sport', 'Mecha', 'Musical','Other']} onChange={handleGenreChange} />
+            <FormInput label="Issue Number" type="text" placeholder="Enter issue number" name="issue_num" value={formData.issue_num} onChange={handleChange} />
+            <FormInput label="Username" type="text" placeholder="Enter username" name="user_id" value={formData.user_id} onChange={handleChange} />
             <FormRating label="Rating" name="rating" value={formData.rating} onChange={handleChange} min={0} max={5} step={0.1} />
             <FormSelect label="Category" name="category" value={formData.category} options={[{ value: 'Book', label: 'Book' }, { value: 'Comic Book', label: 'Comic Book' }, { value: 'Game', label: 'Game' }]} onChange={handleChange} />
             <Button variant="primary" type="submit">Submit</Button>

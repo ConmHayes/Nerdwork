9944e91b5a8d245d8ef62ca35310538060f06eed
Bruno <brunoradoja2911@gmail.com>
2023-11-21 16:55:11 +0000

    testing started

--- client/src/components/FormSelect/index.jsx
+++ client/src/components/FormSelect/index.jsx
@@ -4,9 +4,9 @@ import { Form } from 'react-bootstrap';
 const FormSelect = ({ label, name, value, options, onChange, disabled = false }) => {
   return (
     <Form.Group className="mb-3">
-      <Form.Label>{label}</Form.Label>
-      <Form.Control as="select" name={name} value={value} onChange={onChange} disabled={disabled}>
-        <option value="" disabled style={{ color: '#666' }}>Select...</option> {/* Unselectable and darker */}
+      <Form.Label htmlFor={name}>{label}</Form.Label> {/* Updated to use htmlFor */}
+      <Form.Control as="select" name={name} id={name} value={value} onChange={onChange} disabled={disabled}>
+        <option value="" disabled style={{ color: '#666' }}>Select...</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}

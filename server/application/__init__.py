25ae627f13a4d5aed327da45e71d568aff9145d7
Connor Hayes <conmhayes@gmail.com>
2023-11-21 09:27:52 +0000

    database reset fixing a few errors

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # from application import routes
-from application.routes import auth, user, item, find_data, community
+from application.routes import auth, user, item, find_data, community, thread
 
 
 app = Flask(__name__)
@@ -29,4 +29,5 @@ app.register_blueprint(user.user_bp)
 app.register_blueprint(item.item_bp)
 app.register_blueprint(find_data.google_bp)
 app.register_blueprint(community.community_bp)
+app.register_blueprint(thread.thread_bp)
 
